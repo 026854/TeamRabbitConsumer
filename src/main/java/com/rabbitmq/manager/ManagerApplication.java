9fddc9df2df8da1001474ed67d02747219f0f8d2
mozza55 <mozzarella1103@gmail.com>
2020-05-25 17:36:02 +0900

    <DOING> netty 적용중

--- src/main/java/com/rabbitmq/manager/ManagerApplication.java
+++ src/main/java/com/rabbitmq/manager/ManagerApplication.java
@@ -1,14 +1,25 @@
 package com.rabbitmq.manager;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class ManagerApplication {
+public class ManagerApplication{
+
+    @Autowired
+    private ApplicationContext context;
 
     public static void main(String[] args) {
-        SpringApplication.run(ManagerApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(ManagerApplication.class, args);
+
+        NettyServer nettyServer = context.getBean(NettyServer.class);
+        nettyServer.start();
     }
+
 
 }
